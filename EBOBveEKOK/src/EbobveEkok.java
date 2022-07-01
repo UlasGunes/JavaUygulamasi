33351a07db1198f942b4d1a16a997a411ae175f3
UlasGunes <89416043+UlasGunes@users.noreply.github.com>
2022-07-01 15:43:56 +0300

    EBOB ve EKOK Bulma
    EBOB ve EKOK Bulma

---
+++ EBOBveEKOK/src/EbobveEkok.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class EbobveEkok {
+    public static void main(String[] args) {
+        int a, b, ekok = 0;
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Birinci sayiyi giriniz:");
+        a = inp.nextInt();
+        System.out.print("Ikinci sayiyi giriniz:");
+        b = inp.nextInt();
+        int toplam = a * b;
+        int x = a;
+
+        while (a > 0) {
+            if ((a % x == 0) && (b % x == 0)) {
+                System.out.println("ebob : " + x);
+                break;
+            }else
+                x--;
+        }
+        while (a > 0 && b > 0) {
+            ekok = (a * b) / x;
+            System.out.println("ekok: " + ekok);
+            break;
+        }
+    }
+}
